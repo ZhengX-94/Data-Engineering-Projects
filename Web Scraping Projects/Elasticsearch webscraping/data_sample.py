45cc4adb6ff9802f59b945429974699b877f7f8b
ZhengX-94 <54282019+ZhengX-94@users.noreply.github.com>
2020-03-31 23:38:08 -0400

    added es scraping dir

---
+++ Web Scraping Projects/Elasticsearch webscraping/data_sample.py
@@ -0,0 +1,79 @@
+https://github.com/jvandenaardweg/linkedin-profile-scraper
+
+{
+  "userProfile": {
+    "fullName": "Barack Obama",
+    "title": "Former President of the United States of America",
+    "location": {
+      "city": "Washington D.C. Metro"
+    },
+    "photo": "https://media.licdn.com/dms/image/C4E03AQF2C6iUecWOnQ/profile-displayphoto-shrink_800_800/0?e=1552521600&v=beta&t=s7v_meT4DPvYHiKWdhtuHy_XUHq0DcLu-uKGnbImQjc",
+    "description": "“It falls to each of us to be those anxious, jealous guardians of our democracy; to embrace the joyous task we’ve been given to continually try to improve this great nation of ours. Because for all our outward differences, we all share the same proud title: Citizen.” https://barackobama.com/ https://obamawhitehouse.archives.gov/",
+    "url": "https://www.linkedin.com/in/barackobama/"
+  },
+  "experiences": [
+    {
+      "title": "President",
+      "company": "United States of America",
+      "location": null,
+      "startDate": "2009-01-01T00:00:00+01:00",
+      "endDate": "2017-01-01T00:00:00+01:00",
+      "durationInDays": 2923,
+      "description": "I served as the 44th President of the United States of America."
+    },
+    {
+      "title": "US Senator",
+      "company": "US Senate (IL-D)",
+      "location": null,
+      "startDate": "2005-01-01T00:00:00+01:00",
+      "endDate": "2008-11-01T00:00:00+01:00",
+      "durationInDays": 1401,
+      "description": "In the U.S. Senate, I sought to focus on tackling the challenges of a globalized, 21st century world with fresh thinking and a politics that no longer settles for the lowest common denominator."
+    },
+    {
+      "title": "State Senator",
+      "company": "Illinois State Senate",
+      "location": null,
+      "startDate": "1997-01-01T00:00:00+01:00",
+      "endDate": "2004-01-01T00:00:00+01:00",
+      "durationInDays": 2557,
+      "description": "Proudly representing the 13th District on Chicago's south side."
+    },
+    {
+      "title": "Senior Lecturer in Law",
+      "company": "University of Chicago Law School",
+      "location": null,
+      "startDate": "1993-01-01T00:00:00+01:00",
+      "endDate": "2004-01-01T00:00:00+01:00",
+      "durationInDays": 4018,
+      "description": null
+    }
+  ],
+  "education": [
+    {
+      "schoolName": "Harvard University",
+      "degreeName": "Juris Doctor",
+      "fieldOfStudy": "Law",
+      "startDate": "1988-01-01T00:00:00+01:00",
+      "endDate": "1991-01-01T00:00:00+01:00",
+      "durationInDays": 1097
+    },
+    {
+      "schoolName": "Columbia University in the City of New York",
+      "degreeName": "Bachelor of Arts",
+      "fieldOfStudy": "Political Science, concentration in International Relations",
+      "startDate": "1981-01-01T00:00:00+01:00",
+      "endDate": "1983-01-01T00:00:00+01:00",
+      "durationInDays": 731
+    },
+    {
+      "schoolName": "Occidental College",
+      "degreeName": null,
+      "fieldOfStudy": "Political Science",
+      "startDate": "1979-01-01T00:00:00+01:00",
+      "endDate": "1981-01-01T00:00:00+01:00",
+      "durationInDays": 732
+    }
+  ],
+  "skills": []
+}
